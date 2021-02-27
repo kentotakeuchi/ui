0e23bbc62bc9a29f5686ea3025dccd63a53f513b
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-02-27 00:14:16 -0800

    initial commit: ~ scafffold

---
+++ packages/button/src/button.stories.js
@@ -0,0 +1,8 @@
+import Button from './button';
+
+export default {
+  component: Button,
+  title: 'Design System/Button',
+};
+
+export const primary = () => <Button>{'Button'}</Button>;
---
+++ packages/input/src/input.stories.js
@@ -0,0 +1,8 @@
+import Input from './input';
+
+export default {
+  component: Input,
+  title: 'Design System/Input',
+};
+
+export const placeholder = () => <Input placeholder="user@gmail.com" />;
